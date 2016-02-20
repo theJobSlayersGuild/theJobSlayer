3de92479c62894720e506d87300285c891c58b73
Richard Reynolds <rareynolds84@gmail.com>
2016-02-20 15:08:23 -0700

    added endpoints to server file and basic skeleton to backend ctrl files.

---
+++ core/server/controllers/heroCtrl.js
@@ -0,0 +1,23 @@
+var Hero = require('../models/hero.model.js');
+
+module.exports = {
+    createHero: function (req, res) {
+        
+    },
+    
+    loginHero: function (req, res) {
+        
+    },
+    
+    getHero: function (req, res) {
+        
+    },
+    
+    updateHero: function (req, res) {
+        
+    },
+    
+    deleteHero: function (req, res) {
+        
+    }
+}
---
+++ core/server/controllers/stepCtrl.js
@@ -0,0 +1,19 @@
+var Step = require('../models/step.model.js');
+
+module.exports = {
+       createStep: function (req, res) {
+           
+       },
+       
+       getStep: function (req, res) {
+           
+       },
+       
+       updateStep: function (req, res) {
+           
+       },
+       
+       deleteStep: function (req, res) {
+           
+       }
+}
