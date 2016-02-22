e89ad72784b7f8d2ba1815fb3ae23a214bd55ca7
Richard Reynolds <rareynolds84@gmail.com>
2016-02-22 11:37:02 -0700

    Did stuff

--- core/server/server.js
+++ core/server/server.js
@@ -29,43 +29,43 @@ var resourceCtrl = require('./controllers/resourceCtrl')
 
 //ENDPOINTS
                 //AUTHENTICATION
-app.post(‘api/hero/signUp’, heroCtrl.createHero)
-app.post(‘api/hero/login’, heroCtrl.loginHero)
+app.post('api/hero/signUp', heroCtrl.createHero)
+app.post('api/hero/login', heroCtrl.loginHero)
 
                 //HERO
-app.get(‘api/hero/:id’, heroCtrl.getHero)
-app.put(‘api/hero/:id’, heroCtrl.editHero)
-app.delete(‘api/hero/:id’, heroCtrl.deleteHero)
+app.get('api/hero/:id', heroCtrl.getHero)
+app.put('api/hero/:id', heroCtrl.editHero)
+app.delete('api/hero/:id', heroCtrl.deleteHero)
 
                 //JOB
-app.post(‘api/job/new, jobCtrl.createJob)
-app.get(‘api/job/:id’, jobCtrl.getJobs)
-app.put(‘api/job/:id’, jobCtrl.editJob)
-app.delete(‘api/job/:id’, jobCtrl.deleteJob)
+app.post('api/job/new', jobCtrl.createJob)
+app.get('api/job/:id', jobCtrl.getJobs)
+app.put('api/job/:id', jobCtrl.editJob)
+app.delete('api/job/:id', jobCtrl.deleteJob)
 
                 //GUILD
-app.post(‘api/guild/new’, guildCtrl.createGuild)
-app.get(‘api/guild/:id’, guildCtrl.getGuild)
-app.put(‘api/guild/:id’, guildCtrl.editGuild)
-app.delete(‘api/guild/:id’, guildCtrl.deleteGuild)
+app.post('api/guild/new', guildCtrl.createGuild)
+app.get('api/guild/:id', guildCtrl.getGuild)
+app.put('api/guild/:id', guildCtrl.editGuild)
+app.delete('api/guild/:id', guildCtrl.deleteGuild)
 
                 //QUEST
-app.post(‘api/quest/new’, questCtrl.addQuest)
-app.get(‘api/quest/:id’, questCtrl.getQuest)
-app.put(‘api/quest/:id’, questCtrl.updateQuest)
-app.delete(‘api/quest/:id’, questCtrl.deleteQuest)
+app.post('api/quest/new', questCtrl.addQuest)
+app.get('api/quest/:id', questCtrl.getQuest)
+app.put('api/quest/:id', questCtrl.updateQuest)
+app.delete('api/quest/:id', questCtrl.deleteQuest)
 
                 //STEP
-//app.post(‘api/step/new’, stepCtrl.createStep)
-app.get(‘api/step/:id’, stepCtrl.getStep)
-app.put(‘api/step/:id’, stepCtrl.editStep)
-app.delete(‘api/step/:id’, stepCtrl.deleteStep)
+//app.post('api/step/new', stepCtrl.createStep)
+app.get('api/step/:id', stepCtrl.getStep)
+app.put('api/step/:id', stepCtrl.editStep)
+app.delete('api/step/:id', stepCtrl.deleteStep)
 
                 //RESOURCE
-app.post(‘api/resource/new’, resourceCtrl.addResource)
-app.get(‘api/resource/:id’, resourceCtrl.getResource)
-app.put(‘api/resource/:id’, resourceCtrl.editResource)
-app.delete(‘api/resource/:id’, resourceCtrl.deleteResource)
+app.post('api/resource/new', resourceCtrl.addResource)
+app.get('api/resource/:id', resourceCtrl.getResource)
+app.put('api/resource/:id', resourceCtrl.editResource)
+app.delete('api/resource/:id', resourceCtrl.deleteResource)
 
 
 var port = 8008;
