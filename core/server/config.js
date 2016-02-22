246e1fac7b1ce1866a2a72c0e547e6b2817ba4b9
3chordme <3chordme@gmail.com>
2016-02-22 11:44:36 -0700

    Removed an unnecessary space

--- core/server/config.js
+++ core/server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: 3000,
-  MONGO_URI: 'mongodb://3chordme:w3bdev@ds013898.mongolab.com:13898/the_job_slayer' || 'mongodb://localhost:27017/theJobSlayer' 
+  MONGO_URI: 'mongodb://3chordme:w3bdev@ds013898.mongolab.com:13898/the_job_slayer' || 'mongodb://localhost:27017/theJobSlayer'
 };
