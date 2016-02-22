c7af9ce07f1e9a10df6c3ba31642f08fecdd15b7
Richard Reynolds <rareynolds84@gmail.com>
2016-02-22 12:28:37 -0700

    add CRUD functions to server controllers

--- core/server/controllers/jobCtrl.js
+++ core/server/controllers/jobCtrl.js
@@ -3,19 +3,39 @@ var Job = require('../models/job.model.js');
 module.exports = {
 
     createJob: function (req, res) {
-
+        Job.create(req.body, function (err, result) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.status(200).send(result);
+        })
     },
 
     readJob: function (req, res) {
-
+        Job.find(req.query, function(err, result) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.status(200).send(result)
+        })
     },
 
     updateJob: function (req, res) {
-
+        Job.findByIdAndUpdate(req.params.id, req.body, function(err, result) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.status(200).send(result);
+        })
     },
 
     deleteJob: function (req, res) {
-
+        Job.findByIdAndRemove(req.params.id, function (err, result) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.status(200).send(result);
+        })
     }
 
 };
--- core/server/controllers/questCtrl.js
+++ core/server/controllers/questCtrl.js
@@ -1,21 +1,42 @@
 var Quest = require('../models/quest.model.js');
 
 module.exports = {
-  
-    createQuest: function (req, res) {
 
+    createQuest: function (req, res) {
+        Quest.create(req.body, function (err, result) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.status(200).send(result);
+        })
     },
 
     readQuest: function (req, res) {
-
+        Quest.find(req.query, function(err, result) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.status(200).send(result)
+        })
     },
 
     updateQuest: function (req, res) {
-
+        Quest.findByIdAndUpdate(req.params.id, req.body, function(err, result) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.status(200).send(result);
+        })
     },
 
     deleteQuest: function (req, res) {
-
+        Quest.findByIdAndRemove(req.params.id, function (err, result) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.status(200).send(result);
+        })
     }
+
 
 };
