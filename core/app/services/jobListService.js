89f699ac24104ea5700cd2259f13b16120fc8ede
Brandon Hebbert <brandonhebbert@gmail.com>
2016-02-22 13:20:36 -0700

    create jobs view

---
+++ core/app/services/jobListService.js
@@ -0,0 +1,27 @@
+/**
+ * Created by brandonhebbert on 2/22/16.
+ */
+angular.module('app')
+    .service('jobListService', function ($http) {
+
+        this.getQuests = function() {
+            return $http({
+                method: 'GET',
+                url: 'api/job/:id'
+            }).then(function(response) {
+                return response.data
+            })
+        }
+
+        this.acceptQuest = function(quest) {
+            return $http({
+                method: 'POST',
+                url: '/api/quest/new',
+                data: quest
+
+            }).then(function(response) {
+                return response.data;
+            });
+        };
+
+    });
--- core/app/states/jobs/jobs.ctrl.js
+++ core/app/states/jobs/jobs.ctrl.js
@@ -1,7 +1,23 @@
 var app = angular.module('app');
 
-app.controller('jobsCtrl', function ($scope) {
+app.controller('jobsCtrl', function ($scope, jobListService) {
+
 
     $scope.test = 'jobsCtrl connected';
+
+    $scope.getJobs = function() {
+        jobListService.getQuests()
+            .then(function(response) {
+                $scope.jobs = response;
+            })
+    }
+
+    $scope.acceptJob = function(job, hero) {
+        jobListService.acceptQuest({_job:job, _hero:hero})
+    }
+
+    $scope.editJob = function(job, hero) {
+        jobListService
+    }
 
 });
