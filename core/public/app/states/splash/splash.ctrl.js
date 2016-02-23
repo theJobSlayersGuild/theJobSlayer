12eb9727fdd67169a98917ee146dfaa26c511e3e
Brandon Hebbert <brandonhebbert@gmail.com>
2016-02-22 17:19:45 -0700

    login

--- core/public/app/states/splash/splash.ctrl.js
+++ core/public/app/states/splash/splash.ctrl.js
@@ -1,6 +1,10 @@
 
-app.controller('splashCtrl', function ($scope) {
+app.controller('splashCtrl', function ($scope, jobListService) {
 
     $scope.test = 'splashCtrl connected'
+
+    $scope.submitLogin = function(hero) {
+        jobListService.submitHero(hero)
+    }
 
 });
