f7b98c94aa049dc86bcde97acec84108dd43dfb6
3chordme <3chordme@gmail.com>
2016-02-23 15:22:28 -0700

    Log out is theoretically working

--- core/public/app/services/auth.service.js
+++ core/public/app/services/auth.service.js
@@ -23,4 +23,14 @@ angular.module('app')
         });
     };
 
+    this.logout = function() {
+      return $http({
+        method: 'GET',
+        url: '/api/hero/logout'
+      })
+      .then(function(response) {
+        return response;
+      });
+    };
+
   });
