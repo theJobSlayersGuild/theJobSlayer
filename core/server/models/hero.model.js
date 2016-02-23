6980b41beaa6d3b501e2219c99e414599415bf15
3chordme <3chordme@gmail.com>
2016-02-23 14:25:38 -0700

    Job model updated

--- core/server/models/hero.model.js
+++ core/server/models/hero.model.js
@@ -6,6 +6,7 @@ var heroSchema = mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
     name: { type: String, required: true },
+    title: { type: String, default: "the Barbarian" },
     dateCreated: { type: Date, default: new Date() },
     xp: { type: Number, default: 0 },
     level: { type: Number, default: 1 },
