0636d93021a6798772bd3d3d854d12ee7ce59c2a
Ty Hill <crazysnoheadman@yahoo.com>
2016-02-26 11:17:34 -0700

    added feature to restore default avatar

--- core/public/app/modals/editImage/editimage.ctrl.js
+++ core/public/app/modals/editImage/editimage.ctrl.js
@@ -1,7 +1,12 @@
 angular.module("app").controller("editImageCtrl", function($scope, close) {
 
   $scope.editImage = function(image) {
-    close(image);
+    if (!image) {
+      image = './assets/images/angry_unicorn.png';
+      close(image);
+    } else {
+      close(image);
+    }
   };
 
   $scope.close = close;
