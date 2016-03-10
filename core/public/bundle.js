7ae6870bdc2a3b7187d81d5583a5cf5b4ecdf908
Richard Reynolds <rareynolds84@gmail.com>
2016-03-09 23:54:02 -0700

    matched splash background with other pages

--- core/public/bundle.js
+++ core/public/bundle.js
@@ -798,27 +798,6 @@ angular.module('app')
     });
 
 angular.module('app')
-  .directive('dirNavBar', function() {
-    return {
-      restrict: 'AE',
-      templateUrl: './app/directives/navbar/navbar.dir.html',
-      scope: {
-        hero: '='
-      },
-      controller: ["$scope", "$state", "authService", function($scope, $state, authService) {
-
-        $scope.logout = function() {
-
-          authService.logout()
-            .then(function(response) {
-            });
-          $state.go('splash');
-        };
-      }]
-    };
-  });
-
-angular.module('app')
   .directive('dirMemberResources', function() {
     return {
       restrict: 'AE',
@@ -847,6 +826,27 @@ angular.module('app')
           });
         };
 
+      }]
+    };
+  });
+
+angular.module('app')
+  .directive('dirNavBar', function() {
+    return {
+      restrict: 'AE',
+      templateUrl: './app/directives/navbar/navbar.dir.html',
+      scope: {
+        hero: '='
+      },
+      controller: ["$scope", "$state", "authService", function($scope, $state, authService) {
+
+        $scope.logout = function() {
+
+          authService.logout()
+            .then(function(response) {
+            });
+          $state.go('splash');
+        };
       }]
     };
   });
